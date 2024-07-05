073655f9a6abc9a3526543fe53e13aab9044a822
Vedant Patel <vedantpatel1827@gmail.com>
2024-07-05 10:03:12 +0530

    Config File added

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  // static const String baseURL = "http://192.168.43.21:5000";
+  static const String baseURL = "https://billapp-flask-y2ix.onrender.com";
+  //  static const String baseURL = "postgres://avnadmin:AVNS_Di2nvhA25vvHxQCXWz_@pg-4ebef31-vedantpatelbillapp1827-cea2.c.aivencloud.com:19937/defaultdb?sslmode=require";
+}
